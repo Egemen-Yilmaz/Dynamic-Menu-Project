44b668fb1da66ebd25c767179093a093ba8d3afa
Losrias <egemenylmz01@gmail.com>
2023-03-06 19:12:59 +0300

    first commit

---
+++ src/Menu.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Menu = ({ items }) => {
+  return (
+    <div className="section-center">
+      {items.map((menuItem) => {
+        const { id, title, category, price, img, desc } = menuItem;
+        return (
+          <article className="menu-item" key={id}>
+            <img className="photo" src={img} alt={title}></img>
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">${price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Menu;
